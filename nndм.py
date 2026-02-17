22ec3a9dc2a49cbfb21a90c848b324ef72de796c
ivangnezdilov98-dev <ivangnezdilov98@gmail.com>
2026-02-17 11:03:11 +0500

    Update nndм.py

--- nndм.py
+++ nndм.py
@@ -677,7 +677,7 @@ def categories_kb() -> InlineKeyboardMarkup:
     )
     return builder.as_markup()
 
-def products_kb(category_id: int, page: int = 0, items_per_page: int = 5) -> InlineKeyboardMarkup:
+def products_kb(category_id: int, page: int = 0, items_per_page: int = 20) -> InlineKeyboardMarkup:
     """Товары в категории с пагинацией"""
     builder = InlineKeyboardBuilder()
     products = db.get_products_by_category(category_id)
@@ -3297,4 +3297,5 @@ async def main():
 
 if __name__ == "__main__":
     # Запускаем бота
+
     asyncio.run(main())
