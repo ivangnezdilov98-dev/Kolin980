f0474c0a093d138758c393f908ed3e6248c44477
ivangnezdilov98-dev <ivangnezdilov98@gmail.com>
2026-02-21 23:18:50 +0500

    Update nndм.py

--- nndм.py
+++ nndм.py
@@ -757,7 +757,7 @@ def categories_kb() -> InlineKeyboardMarkup:
     )
     return builder.as_markup()
 
-def products_kb(category_id: int, page: int = 0, items_per_page: int = 10) -> InlineKeyboardMarkup:
+def products_kb(category_id: int, page: int = 0, items_per_page: int = 20) -> InlineKeyboardMarkup:
     """Товары в категории с пагинацией"""
     builder = InlineKeyboardBuilder()
     products = db.get_products_by_category(category_id)
